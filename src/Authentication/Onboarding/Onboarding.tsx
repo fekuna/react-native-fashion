196ff18c6ba61e06ed3c67ca6badacf438cbace7
Alfan Almunawar <almunawar.alfan@gmail.com>
2021-11-25 21:57:01 +0700

    persiapan mengubah redash ke reanimated2

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,10 +1,103 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import Animated from "react-native-reanimated";
+import {
+  useValue,
+  onScrollEvent,
+  interpolateColor,
+} from "react-native-redash/src/v1";
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+
+const BORDER_RADIUS = 75;
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: BORDER_RADIUS,
+  },
+  footer: {
+    flex: 1,
+  },
+  footerContent: {
+    flex: 1,
+    backgroundColor: "white",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+});
+
+const slides = [
+  {
+    title: "Relaxed",
+    color: "#BFEAF5",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfits? Don't worry find the best oufit here",
+  },
+  {
+    title: "Playfull",
+    color: "#BEECC4",
+    subtitle: "Hear it First, Wear it First",
+    description:
+      "Hating the clothes in your wardrobe? Explore hundreds of ourfit ideas",
+  },
+  {
+    title: "Excentric",
+    color: "#FFE4D9",
+    subtitle: "Your Style, Your Way",
+    description:
+      "Create your individuals & unique style and look amazing everyday",
+  },
+  {
+    title: "Funky",
+    color: "#FFDDDD",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Discover the best trends in fashion and explore your personality",
+  },
+];
 
 const Onboarding = () => {
+  const x = useValue(0);
+  const onScroll = onScrollEvent({ x });
+
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
+
   return (
-    <View style={{ flex: 1, backgroundColor: "cyan" }}>
-      <Text>Onboarding</Text>
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map(({ title }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: "cyan",
+          }}
+        ></View>
+        <View
+          style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
+        ></View>
+      </View>
     </View>
   );
 };
